15925b1cbc93e71833a7176ad1326f556ea9b8aa
gujjugando <jugalg05@gmail.com>
2016-04-21 19:05:00 +0530

    Small Changes

--- Assets/Scripts/MakeScriptableObjects.cs
+++ Assets/Scripts/MakeScriptableObjects.cs
@@ -1,42 +1,54 @@
-﻿//using UnityEngine;
-//using System.Collections;
-//using UnityEditor;
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
 
-//public class MakeScriptableObjects{
+public class MakeScriptableObjects{
 
-//	[MenuItem("Assets/Create/New Soldier Properties")]
-//    public static void CreateMySoldier()
-//    {
-//        ScriptableSoldierProps asset = ScriptableObject.CreateInstance<ScriptableSoldierProps>();
-//        AssetDatabase.CreateAsset(asset, "Assets/Data/Soldiers/NewSoldierProps.asset");
-//        AssetDatabase.SaveAssets();
+	[MenuItem("Assets/Create/New Soldier Properties")]
+    public static void CreateMySoldier()
+    {
+        ScriptableSoldierProps asset = ScriptableObject.CreateInstance<ScriptableSoldierProps>();
+        AssetDatabase.CreateAsset(asset, "Assets/Data/Soldiers/NewSoldierProps.asset");
+        AssetDatabase.SaveAssets();
 
-//        EditorUtility.FocusProjectWindow();
+        EditorUtility.FocusProjectWindow();
 
-//        Selection.activeObject = asset;
-//    }
+        Selection.activeObject = asset;
+    }
 
-//    [MenuItem("Assets/Create/New Tower Properties")]
-//    public static void CreateMyTower()
-//    {
-//        ScriptableTowerProps asset = ScriptableObject.CreateInstance<ScriptableTowerProps>();
-//        AssetDatabase.CreateAsset(asset, "Assets/Data/Towers/NewTowerProps.asset");
-//        AssetDatabase.SaveAssets();
+    [MenuItem("Assets/Create/New Tower Properties")]
+    public static void CreateMyTower()
+    {
+        ScriptableTowerProps asset = ScriptableObject.CreateInstance<ScriptableTowerProps>();
+        AssetDatabase.CreateAsset(asset, "Assets/Data/Towers/NewTowerProps.asset");
+        AssetDatabase.SaveAssets();
 
-//        EditorUtility.FocusProjectWindow();
+        EditorUtility.FocusProjectWindow();
 
-//        Selection.activeObject = asset;
-//    }
+        Selection.activeObject = asset;
+    }
 
-//    [MenuItem("Assets/Create/New Rank Properties")]
-//    public static void CreateMyRank()
-//    {
-//        ScriptableRankProperties asset = ScriptableObject.CreateInstance<ScriptableRankProperties>();
-//        AssetDatabase.CreateAsset(asset, "Assets/Data/Ranks/NewRankProps.asset");
-//        AssetDatabase.SaveAssets();
+    [MenuItem("Assets/Create/New Rank Properties")]
+    public static void CreateMyRank()
+    {
+        ScriptableRankProperties asset = ScriptableObject.CreateInstance<ScriptableRankProperties>();
+        AssetDatabase.CreateAsset(asset, "Assets/Data/Ranks/NewRankProps.asset");
+        AssetDatabase.SaveAssets();
 
-//        EditorUtility.FocusProjectWindow();
+        EditorUtility.FocusProjectWindow();
 
-//        Selection.activeObject = asset;
-//    }
-//}
+        Selection.activeObject = asset;
+    }
+
+    [MenuItem("Assets/Create/New King1 Properties")]
+    public static void CreateMyKing()
+    {
+        ScriptableKingProperties asset = ScriptableObject.CreateInstance<ScriptableKingProperties>();
+        AssetDatabase.CreateAsset(asset, "Assets/Data/King1/NewKingProps.asset");
+        AssetDatabase.SaveAssets();
+
+        EditorUtility.FocusProjectWindow();
+
+        Selection.activeObject = asset;
+    }
+}
--- Assets/Scripts/StoreManager.cs
+++ Assets/Scripts/StoreManager.cs
@@ -6,6 +6,7 @@ using System;
 public class StoreManager : MonoBehaviour {
 
     public GameObject[] soldierPanels;
+    public GameObject[] kingPanels;
     public Text gems;
     public Text coins;
 
